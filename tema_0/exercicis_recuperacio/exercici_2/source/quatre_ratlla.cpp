5cf804bef0f3d7d5a4ee61ddef343433936130d4
evalveny <ernest@cvc.uab.es>
2025-02-22 19:16:57 +0100

    ex. recuperació 2

--- tema_0/exercicis_recuperacio/exercici_2/source/quatre_ratlla.cpp
+++ tema_0/exercicis_recuperacio/exercici_2/source/quatre_ratlla.cpp
@@ -12,14 +12,7 @@ int posaFitxa(int tauler[N_FILES][N_COLUMNES], int columna, int jugador)
 
 bool quatreRatlla(int tauler[N_FILES][N_COLUMNES], int fila, int columna)
 {
-	bool ratlla = quatreRatllaDireccio(tauler, fila, columna, 1, 0);
-	if (!ratlla)
-		ratlla = quatreRatllaDireccio(tauler, fila, columna, 0, 1);
-		if (!ratlla)
-			ratlla = quatreRatllaDireccio(tauler, fila, columna, 1, 1);
-			if (!ratlla)
-				ratlla = quatreRatllaDireccio(tauler, fila, columna, -1, 1);
-	return ratlla;
+	
 }
 
 
